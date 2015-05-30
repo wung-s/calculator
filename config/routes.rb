04ddcf9862b53199f29e9a034803df38440447ad
wung <wungmathing@gmail.com>
2015-05-31 00:56:50 +0530

    make a basic user model with secure passwords

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'static_pages/all'
 
   # The priority is based upon order of creation: first created -> highest priority.
---
+++ db/migrate/20150530153341_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.integer :role
+
+      t.timestamps null: false
+
+      add_index :users, :email, unique: true
+    end
+  end
+end
